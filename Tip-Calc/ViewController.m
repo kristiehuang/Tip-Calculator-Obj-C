fd9627fc73cf14434790cf87dfd270f28c99f096
Kristie Huang <kristiehuang@fb.com>
2020-06-23 15:39:43 -0700

    Basic working tip calc

--- Tip-Calc/ViewController.m
+++ Tip-Calc/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -19,5 +23,21 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"hello world"); //@ to make NSString obj-c not C string
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
 
 @end
